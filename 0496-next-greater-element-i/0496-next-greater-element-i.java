db9453aeb128358762ede801018267f64d9f1ad0
Shubham Agarwal <70379049+Shubh-debug@users.noreply.github.com>
2025-09-12 02:47:13 +0530

    Time: 2 ms (99.34%), Space: 44.5 MB (26.62%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,31 +1,32 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        int len1=nums1.length;
-        int len2=nums2.length;
-        int ans[] = new int[len1];
-        for (int i=0; i<len2; i++)
+
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        int ans[] = new int[len1]; //equal length of num1 and answer
+
+        for(int i = 0; i < len2; i++) //outer loop for 2nd array running and storing key values in the hashmap
         {
             map.put(nums2[i], i);
         }
-        for (int j=0; j<len1; j++)
-        { ans[j]=-1;
-            if(map.containsKey(nums1[j]))
+        for(int j = 0; j < len1; j++)
+        {
+            ans[j] = -1;
+            if (map.containsKey(nums1[j]))
             {
-                int index= map.get(nums1[j]);
-               
-                for(int i=index+1;i<len2;i++)
+            int index = map.get(nums1[j]);
+            for(int i = index + 1; i < len2; i++)
+            {
+                if(nums2[i] > nums2[index])
                 {
-                    if(nums2[i]>nums2[index])
-                    {
-                        ans[j]=nums2[i];
-                        break;
-                    }
-                        
+                    ans[j] = nums2[i];
+                    break;
                 }
                 
             }
         }
+        }
         return ans;
-    }
+     }
 }
